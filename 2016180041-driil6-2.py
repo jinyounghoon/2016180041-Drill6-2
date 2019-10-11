98e4d3e5f4b83a5d4f333cf2560979ece803f59a
jinyounghoon <jin304802@naver.com>
2019-10-11 14:40:05 +0900

    랜덤 함수 구현
    2016180041-진영훈-drill6.2-커밋횟수 : 2

--- 2016180041-driil6-2.py
+++ 2016180041-driil6-2.py
@@ -13,7 +13,7 @@ def handle_events():
     global running
     global dir
     global z
-    global x
+
 
     events = get_events()
     for event in events:
@@ -28,62 +28,48 @@ def handle_events():
                 z = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-                z = 3
-            elif event.key == SDLK_LEFT:
-                dir += 1
-                z = 2
 
 
 def draw_curve_4_points(p1, p2, p3, p4):
-
+    global x, y
     for i in range(0, 50, 2):
         t = i / 100
         x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
         y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        draw_point((x, y))
-    draw_point(p2)
 
     # draw p2-p3
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
-    draw_point(p3)
 
     # draw p3-p4
     for i in range(50, 100, 2):
         t = i / 100
         x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
         y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-        draw_point((x, y))
-    draw_point(p4)
 
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
         y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
-        draw_point((x, y))
-    draw_point(p4)
 
 
 size = 10
-points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+points = [(random.randint(0, KPU_WIDTH // 2), random.randint(0, KPU_HEIGHT // 2)) for i in range(size)]
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 dir = 0
 z = 0
+n = 1
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
-    character.clip_draw(frame * 100, z * 100, 100, 100, x, 90)
-
+    draw_curve_4_points(points[n], points[n+1], points[n+2], points[n+3])
+    character.clip_draw(frame * 100, z * 100, 100, 100, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
